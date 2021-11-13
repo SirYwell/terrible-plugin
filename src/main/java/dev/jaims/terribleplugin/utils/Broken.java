af7170b95e7d9c0b2379ed2726013a22fb9093c5
SirYwell <hannesgreule@outlook.de>
2021-11-13 10:03:02 +0100

    :rage1:

---
+++ src/main/java/dev/jaims/terribleplugin/utils/Broken.java
@@ -0,0 +1,11 @@
+package dev.jaims.terribleplugin.utils;
+
+public class Broken {
+
+    public static void main(String[] args) {
+        args = null;
+        String a = args[1];
+
+        System.out.println(a.toString().toString());
+    }
+}
